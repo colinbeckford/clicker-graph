5d33d20ad5522bccb272738c799207473c6b1a7f
Colin Beckford <csb92@drexel.edu>
2018-10-03 14:08:43 -0400

    Getting rid of math.round

--- clicker.js
+++ clicker.js
@@ -276,13 +276,13 @@ function convertList(list)
     var min = 0;
     if (list[i][0] > 60)
     {
-      min = Math.round((list[i][0])/60);
-      sec = Math.round((list[i][0])%60);
+      min = ((list[i][0])/60);
+      sec = ((list[i][0])%60);
     }
     else
     {
       min = 0;
-      sec = Math.round(list[i][0]);
+      sec = (list[i][0]);
     }
     if (list[i][0] < 10)
     {
